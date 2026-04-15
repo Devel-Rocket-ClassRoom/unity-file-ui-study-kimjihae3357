b27ebf2099a6ff2902ae97872c04721015b0008a
kjh3357 <bmj1644035@gmail.com>
2026-04-15 16:57:00 +0900

    json 기능 추가 실습

---
+++ UI Study/Assets/Scripts_3/JsonTest1.cs
@@ -0,0 +1,87 @@
+using Newtonsoft.Json;
+using System.IO;
+using UnityEngine;
+using System;
+
+[Serializable]
+
+public class PlayerState
+{
+    
+    public string playerName;
+    public int lives;
+    public float health;
+    
+    public Vector3 position;
+
+    public override string ToString()
+    {
+        return $"{playerName} / {lives} / {health}";
+    }
+}
+
+public class JsonTest1 : MonoBehaviour
+{
+    private JsonSerializerSettings jsonSetting;
+
+    private void Awake()
+    {
+        jsonSetting = new JsonSerializerSettings();
+        jsonSetting.Formatting = Formatting.Indented;
+        jsonSetting.Converters.Add(new  Vector3Converter());
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // Save
+            PlayerState obj = new PlayerState
+            {
+                playerName = "ABE",
+                lives = 10,
+                health = 10.999f,
+
+            };
+
+            string pathFolder = Path.Combine(
+                Application.persistentDataPath,
+                "JsonTest"
+                );
+
+            if (!Directory.Exists(pathFolder))
+            {
+                Directory.CreateDirectory(pathFolder);
+            }
+            string path = Path.Combine(
+                pathFolder,
+                "player2.json"
+                );
+
+            string json = JsonConvert.SerializeObject(obj, jsonSetting);
+            File.WriteAllText(path, json);
+
+            Debug.Log(path);
+            Debug.Log(json);
+
+           
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            // Load
+            string path = Path.Combine(
+                Application.persistentDataPath,
+                "JsonTest",
+                "player2.json"
+                );
+
+            string json = File.ReadAllText(path);
+            PlayerState obj = JsonConvert.DeserializeObject<PlayerState>(
+                json, jsonSetting);
+
+            Debug.Log(json);
+            Debug.Log(obj);
+        }
+    }
+}
---
+++ UI Study/Assets/Scripts_3/JsonTest2.cs
@@ -0,0 +1,121 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class SomeClass
+{
+    public int prefabIndex;
+    public Vector3 pos;
+    public Quaternion rot;
+    public Vector3 scale;
+    public Color color;
+}
+
+[System.Serializable]
+public class SaveData
+{
+    public List<SomeClass> objects = new List<SomeClass>();
+}
+
+public class JsonTest2 : MonoBehaviour
+{
+    public string fileName = "test.json";
+    public string FileFullPath => Path.Combine(Application.persistentDataPath, "JsonTest", fileName);
+
+    public JsonSerializerSettings jsonSettings;
+    public GameObject[] prefabs;
+    public int count = 5;
+
+    private List<GameObject> spawnedObjects = new List<GameObject>();
+    private Dictionary<GameObject, int> spawnedPrefabIndex = new Dictionary<GameObject, int>(); //씬에 있는 오브젝트
+
+    public void Awake()
+    {
+
+        jsonSettings = new JsonSerializerSettings();
+        jsonSettings.Formatting = Formatting.Indented;
+        jsonSettings.Converters.Add(new Vector3Converter());
+        jsonSettings.Converters.Add(new QuaternionConverter());
+        jsonSettings.Converters.Add(new ColorConverter());
+
+    }
+    public void Save()
+    {
+        string folderPath = Path.GetDirectoryName(FileFullPath);
+        Directory.CreateDirectory(folderPath);
+
+        SaveData saveData = new SaveData();
+
+        for (int i = 0; i < spawnedObjects.Count; i++)
+        {
+            SomeClass data = new SomeClass();
+            GameObject obj = spawnedObjects[i];
+
+            data.prefabIndex = spawnedPrefabIndex[obj];
+            data.pos = obj.transform.position;
+            data.rot = obj.transform.rotation;
+            data.scale = obj.transform.localScale;
+            data.color = obj.GetComponent<Renderer>().material.color;
+            saveData.objects.Add(data);
+
+        }
+        var json = JsonConvert.SerializeObject(saveData, jsonSettings);
+        File.WriteAllText(FileFullPath, json);
+        Debug.Log("세이브 완료");
+    }
+
+    public void Load()
+    {
+        string json = File.ReadAllText(FileFullPath);
+        SaveData saveData = JsonConvert.DeserializeObject<SaveData>(json, jsonSettings);
+
+        for (int i = 0; i < spawnedObjects.Count; i++)
+        {
+            Destroy(spawnedObjects[i]);
+        }
+        spawnedObjects.Clear();
+        spawnedPrefabIndex.Clear();
+
+        for (int i = 0; i < saveData.objects.Count; i++)
+        {
+            SomeClass data = saveData.objects[i];
+            GameObject obj = Instantiate(prefabs[data.prefabIndex], data.pos, data.rot);
+            obj.transform.localScale = data.scale;
+            obj.GetComponent<Renderer>().material.color = data.color;
+
+            spawnedObjects.Add(obj);
+            spawnedPrefabIndex[obj] = data.prefabIndex;
+        }
+        Debug.Log("로드 완료");
+    }
+
+    public void Create()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 spawnpos = new Vector3(
+                Random.Range(-5f, 5f),
+                Random.Range(-5f, 5f),
+                0f
+            );
+
+            int randomIndex = Random.Range(0, prefabs.Length);
+            GameObject obj = Instantiate(prefabs[randomIndex], spawnpos, Quaternion.identity);
+            spawnedObjects.Add(obj);
+            spawnedPrefabIndex[obj] = randomIndex;
+        }
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < spawnedObjects.Count; i++)
+            Destroy(spawnedObjects[i]);
+        spawnedObjects.Clear();
+        spawnedPrefabIndex.Clear();
+
+        Debug.Log("전체 삭제");
+    }
+
+}
